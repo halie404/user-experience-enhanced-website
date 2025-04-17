2b4da6ed0dcdfb21a02bcdc691b4482fc5ff996d
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-04-17 20:53:27 +0200

    #2 start server setup api urls ect.

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-console.log('Hier komt je server voor Sprint 10.')
+import express from "express";
+import { Liquid } from "liquidjs";
+import fetch from "node-fetch";
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-console.log('Zet \'m op!')
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+app.set("view engine", "liquid");
+app.set("views", "./views");
+
+// --- base api url ---
+const directusApiBaseUrl = "https://fdnd-agency.directus.app/items";
+
+// --- endpoints for webinars + bookmarks ---
+const webinarsEndpoint = `${directusApiBaseUrl}/avl_webinars`;
+const categoriesEndpoint = `${directusApiBaseUrl}/avl_categories`;
+const bookmarksEndpoint = `${directusApiBaseUrl}/avl_messages`;
+
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), () => {
+  console.log(`App is running on http://localhost:${app.get("port")}`);
+});
